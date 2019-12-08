71708fe083acac0bf7d9f730643769b7dfeb8044
Matti Lattu <matti.lattu@iki.fi>
2019-12-08 11:47:39 +0200

    Move parts up/down from Admin UI

--- src/ui/lib/BootstrapIconWrapper.js
+++ src/ui/lib/BootstrapIconWrapper.js
@@ -1,10 +1,10 @@
 class BootstrapIconWrapper {
   constructor() {
-    this.icon_plus = '<!-- include-jsesc:src/ui/ext/icons/plus.svg -->';
+    this.plus = '<!-- include-jsesc:src/ui/ext/icons/plus.svg -->';
     this.chevron_up = '<!-- include-jsesc:src/ui/ext/icons/chevron-up.svg -->';
     this.chevron_down = '<!-- include-jsesc:src/ui/ext/icons/chevron-down.svg -->';
-    this.icon_arrow_up = '<!-- include-jsesc:src/ui/ext/icons/arrow-up.svg -->';
-    this.icon_arrow_down = '<!-- include-jsesc:src/ui/ext/icons/arrow-down.svg -->';
-    this.icon_trash = '<!-- include-jsesc:src/ui/ext/icons/trash.svg -->';
+    this.arrow_up = '<!-- include-jsesc:src/ui/ext/icons/arrow-up.svg -->';
+    this.arrow_down = '<!-- include-jsesc:src/ui/ext/icons/arrow-down.svg -->';
+    this.trash = '<!-- include-jsesc:src/ui/ext/icons/trash.svg -->';
   }
 }
--- src/ui/lib/PageContent.js
+++ src/ui/lib/PageContent.js
@@ -89,8 +89,10 @@ class PageContent {
     html.push('<div class="row"><div class="col-12"><label for="'+name+'" class="label_text">Text</label>'+this.render_editor_input('text', name)+'</div></div>');
     html.push('<div class="row"><div class="col-12">');
     html.push('<button type="button" class="btn btn-secondary btn-sm button_part button_advanced" data-partnumber="'+n+'">'+biw.chevron_down+'</button>');
-    html.push('<button type="button" class="btn btn-secondary btn-sm button_part button_add_part" data-partnumber="'+n+'">'+biw.icon_plus+'</button>');
-    html.push('<button type="button" class="btn btn-danger btn-sm button_part button_delete_part" data-partnumber="'+n+'">'+biw.icon_trash+'</button>');
+    html.push('<button type="button" class="btn btn-secondary btn-sm button_part button_move_down" data-partnumber="'+n+'">'+biw.arrow_down+'</button>');
+    html.push('<button type="button" class="btn btn-secondary btn-sm button_part button_move_up" data-partnumber="'+n+'">'+biw.arrow_up+'</button>');
+    html.push('<button type="button" class="btn btn-secondary btn-sm button_part button_add_part" data-partnumber="'+n+'">'+biw.plus+'</button>');
+    html.push('<button type="button" class="btn btn-danger btn-sm button_part button_delete_part" data-partnumber="'+n+'">'+biw.trash+'</button>');
     html.push('</div></div>');
 
     for (var field in this.fields.section_values) {
@@ -139,8 +141,14 @@ class PageContent {
     $(".section_field").on('keyup', {obj: this}, this.update_object_value);
 
     $(".button_advanced").on("click", {obj: this}, this.button_advanced_toggle);
+    $(".button_move_down").on("click", {obj: this}, this.button_move_down);
+    $(".button_move_up").on("click", {obj: this}, this.button_move_up);
     $(".button_add_part").on("click", {obj: this}, this.button_add_part);
     $(".button_delete_part").on("click", {obj: this}, this.button_delete_part);
+
+
+    $(".button_move_up[data-partnumber=0]").attr("disabled", true);
+    $(".button_move_down[data-partnumber="+(this.get_parts_count()-1)+"]").attr("disabled", true);
   }
 
   advanced_hide() {
@@ -172,6 +180,25 @@ class PageContent {
   part_insert(part) {
     var new_page_data = this.page_data;
     new_page_data.parts.splice(part, 0, {});
+    this.set_data_internal(new_page_data, true);
+  }
+
+  button_move_up(event) {
+    var part = parseInt($(this).attr('data-partnumber'));
+    event.data.obj.part_move(part, part-1);
+  }
+
+  button_move_down(event) {
+    var part = parseInt($(this).attr('data-partnumber'));
+    event.data.obj.part_move(part, part+1);
+  }
+
+  part_move(part_from, part_to) {
+    console.log("move from "+part_from+" to "+part_to);
+    var tmp = this.page_data.parts[part_to];
+    this.page_data.parts[part_to] = this.page_data.parts[part_from];
+    this.page_data.parts[part_from] = tmp;
+    var new_page_data = this.page_data;
     this.set_data_internal(new_page_data, true);
   }
 

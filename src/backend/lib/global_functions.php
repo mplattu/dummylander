2229f1ca83919e7410c5cdb13d06d418f5860cd0
Matti Lattu <matti.lattu@iki.fi>
2019-12-26 22:19:17 +0200

    Remove unnecessary log messages

--- src/backend/lib/global_functions.php
+++ src/backend/lib/global_functions.php
@@ -19,12 +19,10 @@ function remove_trailing_slash($path) {
 
 function get_my_url($url = null) {
   if (is_null($url)) {
-    if ($_SERVER['TEST_MY_URL'] != "") {
-      log_message("Got test variable");
+    if (@$_SERVER['TEST_MY_URL'] != "") {
       $url = $_SERVER['TEST_MY_URL'];
     }
     else {
-      log_message("Did not get test variable");
       $url = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[PHP_SELF]";
     }
   }

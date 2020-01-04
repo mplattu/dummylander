a37228cd5a684d8062f5f556a5b7b2d72b0e8979
Matti Lattu <matti.lattu@iki.fi>
2020-01-04 14:34:01 +0200

    Fix: Clear the password

--- dist/settings.php
+++ dist/settings.php
@@ -1,6 +1,5 @@
 <?php
 /*
-{"ADMIN_PASSWORD":"sKRxLSYa=3uxYKP\u00bdIF7=\"Hs5L7xJ\u00bdO\u00a7BN\u00c53]C(TF%aE%rDHle'eaBKFx9\"pxfhVY=vTU&PPoir\u00a4U\u00a71UzVXyIs\u00e4y'=mxlG\u00e5D5CDH9BLaMaeK66Ph\u00e47\u00c4\/FX"}
+{"ADMIN_PASSWORD":""}
 */
 ?>
-
